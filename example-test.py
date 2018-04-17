0389340123d2a57052c6f7e0c5f61305d8b75953
LMikhaylova <lusya@izba.com>
2018-04-16 21:10:52 -0400

    updates to tests and instructions

--- example-test.py
+++ example-test.py
@@ -1,30 +1,62 @@
+# importing libraries
+import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 class PythonMapTests(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(3)
+    def setUp(self):        
+        self.driver = webdriver.Chrome() #selecting Chrome driver
+        self.actions = ActionChains(self.driver)
+        self.driver.implicitly_wait(3) #setting wait time for actions to complete
+        self.driver.get("https://www.openstreetmap.org") #navigate to the page
 
-    def test_search(self):
+
+    #function returning search input field
+    def getInputBox(self):
         driver = self.driver
-        driver.get("https://www.openstreetmap.org")
+        return driver.find_element(By.ID, "content").find_element(By.ID, "query")
         
-        self.assertIn("OpenStreetMap", driver.title)
-        
-        elem = driver.find_element(By.ID, "content").find_element(By.ID, "query")
-
-        elem.send_keys("Hancock Tower")
-        elem.send_keys(Keys.RETURN)
-
-        elem = driver \
+    #function returning first element of search results
+    def getSearchResultsElement(self):
+        driver = self.driver
+        return driver \
           .find_element(By.CLASS_NAME, "search_results_entry") \
           .find_element(By.TAG_NAME, "p")
+            
+    def test_search_positive(self):
+        driver = self.driver         
+        actions = self.actions
+
+        self.assertIn("OpenStreetMap", driver.title) #verify page Title
+        elem = self.getInputBox()
+
+        elem.send_keys("Hancock Tower") #type the search phrase
+        elem.send_keys(Keys.RETURN) 
+
+        elem = self.getSearchResultsElement()
 
         self.assertNotEqual(elem.text, "No results found")
+        #self.assertEqual(elem.text, "Building John Hancock Tower, Trinity Place, Chinatown, Back Bay, \
+        #Boston, Suffolk County, Massachusetts, 02114, United States of America")
+
+
+        divMap = driver.find_element(By.ID, "map")
+
+        button = driver \
+          .find_element(By.CLASS_NAME, "control-query") \
+          .find_element(By.TAG_NAME, "a")
+
+        button.click()
+        
+        actions.move_to_element(divMap).click().perform()
+
+        time.sleep(5)
+        
 
         urlParts = driver.current_url.split("#map=")
         zoom_lat_lon = urlParts[1].split("/")
@@ -36,9 +68,20 @@ class PythonMapTests(unittest.TestCase):
         self.assertEqual(lat, 42)
         self.assertEqual(lon, -71)
 
-    #def map_click(self):    
+    def test_search_negative(self):
+        driver = self.driver               
+        self.assertIn("OpenStreetMap", driver.title) #verify page Title
+        
+        elem = self.getInputBox()
+        elem.send_keys("fake landmark") #type the search phrase
+        elem.send_keys(Keys.RETURN) 
+
+        elem = self.getSearchResultsElement()
+
+        self.assertEqual(elem.text, "No results found")
 
 
+    
     def tearDown(self):
         self.driver.close()
 
